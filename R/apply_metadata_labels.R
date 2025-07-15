dba3b35d2929411ae7df6e281593c77bd35be908
joewc <joewcorra@gmail.com>
2025-07-15 09:39:01 -0400

    Added library calls

--- R/apply_metadata_labels.R
+++ R/apply_metadata_labels.R
@@ -1,5 +1,8 @@
 library(labelled)
-
+library(readr)
+library(dplyr)
+library(magrittr)
+library(tibble)
 
 # FUNCTION: READ DATA DICTIONARY---------------------------------
 
@@ -10,48 +13,48 @@ ghgi_values <- read_csv("data/data_dictionary_values.csv")
 
 # FUNCTION: APPLY ATTRIBUTE LABELS-------------------------------
 
-# Function: add variable labels to dataframes 
+# Function: add variable labels to dataframes
 apply_variable_labels <- function(data) {
-  
 
-  
+
+
   return(data)
-  
+
 }
 
 apply_labels <- function(df, type = c("columns", "values")) {
-  
+
   type <- match.arg(type)
-  
+
   if (!is.data.frame(df)) {
     stop("`df` must be a dataframe.")
   }
-  
+
   if (type == "columns") {
-    
+
     # Filter the variables dataframe to get the variable labels
     labels <- ghgi_variables %>%
-      dplyr::select(variable, variable_description) %>% 
+      dplyr::select(variable, variable_description) %>%
       dplyr::filter(variable %in%
                names(df)) %>%
-      tibble::deframe() %>% 
+      tibble::deframe() %>%
       as.list()
-    
+
     # Apply the column labels
     labelled_df <- labelled::set_variable_labels(df, .labels = labels)
-    
+
     return(labelled_df)
-    
+
   } else if (type == "values") {
-    
+
     labels <- ghgi_values %>%
-      dplyr::select(value, value_description) %>% 
-      
-      tibble::deframe() %>% 
+      dplyr::select(value, value_description) %>%
+
+      tibble::deframe() %>%
       as.list()
-    
+
     labelled_df <- labelled::set_value_labels(df, .labels = labels)
-    
+
     return(labelled_df)
   }
 }
